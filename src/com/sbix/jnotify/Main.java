e74dbe5b0730ea9f2d5b87bef2705d3c2d1ec503
amechtake <mramechtake@gmail.com>
2013-12-07 19:32:12 +0000

    Update Main.java

--- src/com/sbix/jnotify/Main.java
+++ src/com/sbix/jnotify/Main.java
@@ -8,10 +8,10 @@ public class Main {
 		String str = "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte "
 				+ "standard de l'imprimerie depuis les années 1500, quand un ";
 		
-		new NotifyWindow(NotifyType.DEFAULT_NOTIFICATION, "Le Lorem Ipsum est simplement du faux texte employé",
+		new NotifyWindow(NotifyType.DEFAULT_NOTIFICATION, str,
 				NotifyWindow.NORMAL_DELAY, NPosition.TOP_RIGHT);
 
-		new NotifyWindow(NotifyType.SUCCESS_NOTIFICATION, "Le Lorem Ipsum est simplement du faux texte employé",
+		new NotifyWindow(NotifyType.SUCCESS_NOTIFICATION, str,
 				NotifyWindow.NORMAL_DELAY, NPosition.TOP_RIGHT);
 	}
 }
